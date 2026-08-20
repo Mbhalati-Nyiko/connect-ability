bb0393f341f24c221ef6e8bbf89aad5a3919c647
Nyiko Mbhalati <nyiko9322@gmail.com>
2026-08-20 09:14:57 +0200

    Create digital literacy page. (Standalone: false)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'digital-literacy',
+    loadChildren: () => import('./digital-literacy/digital-literacy.module').then( m => m.DigitalLiteracyPageModule)
   }
 ];
 @NgModule({
---
+++ src/app/digital-literacy/digital-literacy.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-digital-literacy',
+  templateUrl: './digital-literacy.page.html',
+  styleUrls: ['./digital-literacy.page.scss'],
+  standalone: false,
+})
+export class DigitalLiteracyPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
